7f36997992b4f9c525f5964439b3af23573fea6a
Sam Whitehead <spw520@york.ac.uk>
2020-02-17 11:13:21 +0000

    pushp ush

---
+++ desktop/out/production/resources/CampusMap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="CampusMap" tilewidth="32" tileheight="32" tilecount="403" columns="31">
+ <image source="CampusMap.PNG" width="1000" height="437"/>
+</tileset>
---
+++ desktop/out/production/resources/CityMap/tilesetResize.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="tilesetResize" tilewidth="32" tileheight="32" tilecount="168" columns="8">
+ <image source="tileset.v2.png" width="256" height="672"/>
+ <tile id="45">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="71">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+ <tile id="79">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
+ </tile>
+</tileset>
